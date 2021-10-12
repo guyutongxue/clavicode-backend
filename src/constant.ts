5901753503cb75e932ae3e2416f2d215cdf4535b
Guyutongxue <guyutongxue@163.com>
2021-10-12 20:47:27 +0800

    feat(*): temp tokens

---
+++ src/constant.ts
@@ -0,0 +1,20 @@
+// Copyright (C) 2021 Clavicode Team
+// 
+// This file is part of clavicode-backend.
+// 
+// clavicode-backend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// clavicode-backend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with clavicode-backend.  If not, see <http://www.gnu.org/licenses/>.
+
+const TEMP_EXECUTE_TOKEN = "0344a132-6e41-46c9-81b1-08fcb795b0cd";
+const TEMP_CLANGD_TOKEN = "15bf2a54-f439-4874-a252-6a5164a0f559";
+const TEMP_DEBUG_TOKEN = "3a6759cc-2fe4-4d71-a984-b85398ed3f25";
